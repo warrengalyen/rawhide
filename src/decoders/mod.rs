9c2d81366f446d8a628cfb4589aae3778094b6f7
Warren Galyen <wgalyen@gmail.com>
2020-11-07 10:44:08 -0800

    Start first bitwise stuff

--- src/decoders/mod.rs
+++ src/decoders/mod.rs
@@ -1,11 +1,15 @@
 mod mrw;
+mod basics;
 
 pub trait Decoder {
     fn make(&self) -> String;
     fn model(&self) -> String;
 }
 
-pub fn get_decoder<'a>(buffer: &'a [u8]) -> Box<Decoder+'a> {
-    let dec = Box::new(mrw::MrwDecoder::new(buffer));
-    dec as Box<Decoder>
+pub fn get_decoder<'a>(buffer: &'a [u8]) -> Option<Box<Decoder+'a>> {
+    if mrw::is_mrw(buffer) {
+      let dec = Box::new(mrw::MrwDecoder::new(buffer));
+      return Some(dec as Box<Decoder>);
+    }
+    None
 }
