5543ac7583070542b1c274ee5fa6b1c395914a4d
Warren Galyen <wgalyen@gmail.com>
2020-12-05 09:39:08 -0800

    RAF: support old format like the one in the E900

--- src/decoders/raf.rs
+++ src/decoders/raf.rs
@@ -1,47 +1,65 @@
-use decoders::*;
-use decoders::tiff::*;
 use decoders::basics::*;
+use decoders::tiff::*;
+use decoders::*;
 use std::f32::NAN;
 
 #[derive(Debug, Clone)]
 pub struct RafDecoder<'a> {
-  buffer: &'a [u8],
-  rawhide: &'a RawHide,
-  tiff: TiffIFD<'a>,
+    buffer: &'a [u8],
+    rawhide: &'a RawHide,
+    tiff: TiffIFD<'a>,
 }
 
 impl<'a> RafDecoder<'a> {
-  pub fn new(buf: &'a [u8], tiff: TiffIFD<'a>, rawhide: &'a RawHide) -> RafDecoder<'a> {
-    RafDecoder {
-      buffer: buf,
-      tiff: tiff,
-      rawhide: rawhide,
+    pub fn new(buf: &'a [u8], tiff: TiffIFD<'a>, rawhide: &'a RawHide) -> RafDecoder<'a> {
+        RafDecoder {
+            buffer: buf,
+            tiff: tiff,
+            rawhide: rawhide,
+        }
     }
-  }
 }
 
 impl<'a> Decoder for RafDecoder<'a> {
-  fn image(&self) -> Result<Image,String> {
-    let camera = self.rawhide.check_supported(&self.tiff)?;
-    let raw = fetch_ifd!(&self.tiff, Tag::RafOffsets);
-    let width = fetch_tag!(raw, Tag::RafImageWidth).get_u32(0);
-    let height = fetch_tag!(raw, Tag::RafImageLength).get_u32(0);
-    let offset = fetch_tag!(raw, Tag::RafOffsets).get_u32(0) as usize + raw.start_offset();
-    let bps = fetch_tag!(raw, Tag::RafBitsPerSample).get_u32(0);
-    let src = &self.buffer[offset..];
+    fn image(&self) -> Result<Image, String> {
+        let camera = self.rawhide.check_supported(&self.tiff)?;
+        let raw = fetch_ifd!(&self.tiff, Tag::RafOffsets);
+        let (width, height) = if raw.has_entry(Tag::RafImageWidth) {
+            (
+                fetch_tag!(raw, Tag::RafImageWidth).get_u32(0),
+                fetch_tag!(raw, Tag::RafImageLength).get_u32(0),
+            )
+        } else {
+            let sizes = fetch_tag!(raw, Tag::ImageWidth);
+            (sizes.get_u32(1), sizes.get_u32(0))
+        };
+        let offset = fetch_tag!(raw, Tag::RafOffsets).get_u32(0) as usize + raw.start_offset();
+        let bps = match raw.find_entry(Tag::RafBitsPerSample) {
+            Some(val) => val.get_u32(0),
+            None => 16,
+        };
+        let src = &self.buffer[offset..];
 
-    let image = match bps {
-        12 => decode_12le(src, width as usize, height as usize),
-        14 => decode_14le_unpacked(src, width as usize, height as usize),
-        _ => {return Err(format!("RAF: Don't know how to decode bps {}", bps).to_string());},
-      };
-    ok_image(camera, width, height, self.get_wb()?, image)
-  }
+        let image = match bps {
+            12 => decode_12le(src, width as usize, height as usize),
+            14 => decode_14le_unpacked(src, width as usize, height as usize),
+            16 => decode_16le(src, width as usize, height as usize),
+            _ => {
+                return Err(format!("RAF: Don't know how to decode bps {}", bps).to_string());
+            }
+        };
+        ok_image(camera, width, height, self.get_wb()?, image)
+    }
 }
 
 impl<'a> RafDecoder<'a> {
-  fn get_wb(&self) -> Result<[f32;4], String> {
-    let levels = fetch_tag!(self.tiff, Tag::RafWBGRB);
-    Ok([levels.get_f32(1), levels.get_f32(0), levels.get_f32(2), NAN])
-  }
+    fn get_wb(&self) -> Result<[f32; 4], String> {
+        match self.tiff.find_entry(Tag::RafWBGRB) {
+            Some(levels) => Ok([levels.get_f32(1), levels.get_f32(0), levels.get_f32(2), NAN]),
+            None => {
+                let levels = fetch_tag!(self.tiff, Tag::RafOldWB);
+                Ok([levels.get_f32(1), levels.get_f32(0), levels.get_f32(3), NAN])
+            }
+        }
+    }
 }
