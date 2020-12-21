df7ad8470b3ec4040412582be01c6dc6c92829dd
Warren Galyen <wgalyen@gmail.com>
2020-12-21 07:23:54 -0800

    simplify the basic decode API

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,13 @@ extern crate num;
 extern crate itertools;
 
 pub mod decoders;
+use decoders::{RawHide, Image};
 pub mod imageops;
+
+lazy_static! {
+    static ref LOADER: RawHide = decoders::RawHide::new();
+  }
+  
+  pub fn decode(path: &str) -> Result<Image, String> {
+    LOADER.decode_safe(path)
+  }
