23ab2eccb5a9d4da79d86f3688638ca8edc29bbd
Warren Galyen <wgalyen@gmail.com>
2021-01-05 07:36:35 -0800

    Force initialization of lazy_statics when fuzzing AFL hates it when targets vary over time. Forcing lazy_static initialization solves a big stability issue. There are still some cases of variability that I haven't been able to diagnose though.

--- src/lib.rs
+++ src/lib.rs
@@ -47,28 +47,33 @@
   unused_qualifications
 )]
 
-#[macro_use] extern crate enum_primitive;
+#[macro_use]
+extern crate enum_primitive;
 extern crate num;
-#[macro_use] extern crate lazy_static;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate serde_derive;
 extern crate itertools;
 
 mod decoders;
+pub use decoders::cfa::CFA;
+#[doc(hidden)]
+pub use decoders::Buffer;
+pub use decoders::Orientation;
+#[doc(hidden)]
+pub use decoders::RawHide;
 pub use decoders::RawImage;
 pub use decoders::RawImageData;
-pub use decoders::Orientation;
-pub use decoders::cfa::CFA;
-#[doc(hidden)] pub use decoders::Buffer;
-#[doc(hidden)] pub use decoders::RawHide;
 
 lazy_static! {
   static ref LOADER: RawHide = decoders::RawHide::new();
 }
 
-use std::path::Path;
 use std::error::Error;
 use std::fmt;
 use std::io::Read;
+use std::path::Path;
 
 /// Error type for any reason for the decode to fail
 #[derive(Debug)]
@@ -83,43 +88,51 @@ impl fmt::Display for RawHideError {
 }
 
 impl Error for RawHideError {
-    // Implement description so that older versions of rust still work
-    fn description(&self) -> &str {
-      "description() is deprecated; use Display"
-    }
+  // Implement description so that older versions of rust still work
+  fn description(&self) -> &str {
+    "description() is deprecated; use Display"
+  }
 }
 
 impl RawHideError {
   fn new(msg: String) -> Self {
-    Self {
-      msg,
-    }
+    Self { msg }
   }
 }
-  
-  /// Take a path to a raw file and return a decoded image or an error
-  ///
-  /// # Example
-  /// ```rust,ignore
-  /// let image = match rawhide::decode_file("path/to/your/file.RAW") {
-  ///   Ok(val) => val,
-  ///   Err(e) => ... some appropriate action when the file is unreadable ...
-  /// };
-  /// ```
-  pub fn decode_file<P: AsRef<Path>>(path: P) -> Result<RawImage,RawHideError> {
-    LOADER.decode_file(path.as_ref()).map_err(|err| RawHideError::new(err))
-  }
-  
-  /// Take a readable source and return a decoded image or an error
-  ///
-  /// # Example
-  /// ```rust,ignore
-  /// let mut file = match File::open(path).unwrap();
-  /// let image = match rawhide::decode(&mut file) {
-  ///   Ok(val) => val,
-  ///   Err(e) => ... some appropriate action when the file is unreadable ...
-  /// };
-  /// ```
-  pub fn decode(reader: &mut Read) -> Result<RawImage,RawHideError> {
-    LOADER.decode(reader).map_err(|err| RawHideError::new(err))
-  }
+
+// Used to force lazy_static initializations. Useful for fuzzing.
+#[doc(hidden)]
+pub fn force_initialization() {
+  lazy_static::initialize(&LOADER);
+  lazy_static::initialize(&decoders::CRW_HUFF_TABLES);
+  lazy_static::initialize(&decoders::SNEF_CURVE);
+}
+
+/// Take a path to a raw file and return a decoded image or an error
+///
+/// # Example
+/// ```rust,ignore
+/// let image = match rawhide::decode_file("path/to/your/file.RAW") {
+///   Ok(val) => val,
+///   Err(e) => ... some appropriate action when the file is unreadable ...
+/// };
+/// ```
+pub fn decode_file<P: AsRef<Path>>(path: P) -> Result<RawImage, RawHideError> {
+  LOADER
+    .decode_file(path.as_ref())
+    .map_err(|err| RawHideError::new(err))
+}
+
+/// Take a readable source and return a decoded image or an error
+///
+/// # Example
+/// ```rust,ignore
+/// let mut file = match File::open(path).unwrap();
+/// let image = match rawhide::decode(&mut file) {
+///   Ok(val) => val,
+///   Err(e) => ... some appropriate action when the file is unreadable ...
+/// };
+/// ```
+pub fn decode(reader: &mut Read) -> Result<RawImage, RawHideError> {
+  LOADER.decode(reader).map_err(|err| RawHideError::new(err))
+}
