d486fa76049a7dea6f35919707edd30ee672b1e6
Warren Galyen <wgalyen@gmail.com>
2021-01-08 06:36:59 -0800

    Move CRW to the generic huffman code The CRW code was actually the basis for the new generic huffman code. So get rid of the special code in CRW and just reuse the generic code. Since we only use the huff len and not the diffs the cache is disabled on demand for this format.

--- src/lib.rs
+++ src/lib.rs
@@ -124,7 +124,6 @@ pub fn decode(reader: &mut dyn Read) -> Result<RawImage, RawHideError> {
 #[doc(hidden)]
 pub fn force_initialization() {
   lazy_static::initialize(&LOADER);
-  lazy_static::initialize(&decoders::CRW_HUFF_TABLES);
   lazy_static::initialize(&decoders::SNEF_CURVE);
 }
 
